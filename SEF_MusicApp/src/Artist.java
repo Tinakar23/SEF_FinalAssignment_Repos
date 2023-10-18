df673925b66765250e64b3eabfc4e01d3f38b8c3
Tinakar Rajagopal <tinakar23@gmail.com>
2023-10-19 01:37:51 +1100

    The java code for add artist and its unit test

---
+++ SEF_MusicApp/src/Artist.java
@@ -0,0 +1,267 @@
+import java.text.*;
+import java.util.regex.*;
+import java.io.*;
+import java.util.*;
+public class Artist 
+{
+
+	private String ID;
+	private String Name;
+	private String Address;
+	private String Birthdate;
+	private String Bio;
+	private ArrayList<String> Occupations;
+	private ArrayList<String> Genres;
+	private ArrayList<String> Awards;
+	
+	public Artist(String id, String name, String address, String birthdate, String bio,
+			ArrayList<String> occupations, ArrayList<String> genres, ArrayList<String> awards)
+	{
+		ID = id;
+		Name = name;
+		Address = address;
+		Birthdate = birthdate;
+		Bio = bio;
+		Occupations = occupations;
+		Genres = genres;
+		Awards = awards;		
+	}
+	
+	private boolean preConditionsToAddArtist()
+	{
+		boolean isValidArtistId = false;
+		boolean isValidDate = false;
+		boolean isValidAddress = false;
+		boolean isValidBio = false;
+		boolean isValidOccupation = false;
+		boolean isValidAwards = false;
+		boolean isValidGeners = false;
+			String artistIdPatter ="^^[5-9]{3}[A-Z]{5}[!@#$%^&*()_+=<>?/{}\\[\\]|]{2}$";
+			Pattern artistIdRegex = Pattern.compile(artistIdPatter);
+			Matcher artistIdMatcher = artistIdRegex.matcher(this.ID);
+			isValidArtistId = artistIdMatcher.matches();
+			String datePattern = "^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\\d{4}$$";
+			Pattern dateRegex = Pattern.compile(datePattern);
+			Matcher dateMatcher = dateRegex.matcher(this.Birthdate);
+			isValidDate = dateMatcher.matches();
+			String[] addressParts = this.Address.split("\\|");
+			if(addressParts.length == 3)
+			{
+				isValidAddress= true; 
+			}
+			String[] bioParts = this.Bio.split("\\s+");
+			if(bioParts.length >= 10 && bioParts.length <=30)
+			{
+				isValidBio = true;
+			}
+			if(!this.Occupations.isEmpty() && this.Occupations.size()<=5)
+			{
+				isValidOccupation = true;
+			}
+			if(this.Awards.size()<= 3)
+			{   int invalidDataCount = 0;
+				for(String award : this.Awards)
+				{
+					String[] awardParts = award.split(",");
+					if(awardParts.length == 2)
+					{
+						String year = awardParts[0];
+						String title = awardParts[1];
+						String[] titleWords = title.split("\\s+");
+						if((year.length() != 4 || !(titleWords.length >=4 && titleWords.length <=10)))
+						{
+							invalidDataCount++;
+						}
+							
+					}
+					isValidAwards = invalidDataCount == 0 && awardParts.length == 2;
+				}
+			}
+			if(this.Genres.size() >= 2 && this.Genres.size() <= 5 &&!(this.Genres.contains("rock") && this.Genres.contains("pop")))
+			{
+				isValidGeners = true;
+			}
+			
+			return isValidArtistId && isValidDate &&isValidAddress && isValidBio && isValidOccupation && isValidAwards && isValidGeners;
+	}
+	
+	public boolean addArtist()
+	{
+		boolean isAddArtistSuccess = false;
+		boolean validDetails = this.preConditionsToAddArtist();
+		
+			if(validDetails)
+			{
+				File fileObject = new File("fileName.txt");
+				try
+				{
+					if(!fileObject.exists())
+					{
+						fileObject.createNewFile();
+					}
+
+					FileWriter fileWrite= new FileWriter("fileName.txt", true);
+					fileWrite.write("ID: " + this.ID +"\n");
+					fileWrite.write("Name: " + this.Name+"\n");
+					fileWrite.write("Birthdate: " + this.Birthdate+"\n");
+					fileWrite.write("Address: " + this.Address+"\n");
+					fileWrite.write("Bio: " + this.Bio+"\n");
+					this.writeArrayList(this.Occupations, fileWrite, "Occupation: ");
+					this.writeArrayList(this.Genres, fileWrite, "Genres: ");
+					this.writeArrayList(this.Awards, fileWrite, "Awards: ");
+					fileWrite.write("\n");
+					fileWrite.close();
+					isAddArtistSuccess = true;
+				}
+				catch(IOException e)
+				{
+					System.out.println("Error occured");
+				}
+			}
+			return isAddArtistSuccess;
+			
+	}
+	private void writeArrayList(ArrayList<String> list, FileWriter writer, String title)
+	{
+		String element = title;
+		for (int i = 0; i< list.size();i++ )
+		{
+			String seperator = title == "Awards: " ? "|" : ",";
+			element += list.get(i) + seperator;	
+		}
+		try
+		{
+		writer.write(element + "\n");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public boolean updateArtist()
+	{
+		boolean validDetails = this.preConditionsToAddArtist();
+		ArrayList<String> dataFromFile = new ArrayList<String>();
+		try(BufferedReader bufferReader = new BufferedReader(new FileReader("fileName.txt"));
+		FileWriter bufferWriter = new FileWriter("fileName.txt.tmp"))
+		{
+			String line;
+			bufferWriter.write("ID: " + this.ID +"\n");
+			bufferWriter.write("Name: " + this.Name+"\n");
+			bufferWriter.write("Birthdate: " + this.Birthdate+"\n");
+			bufferWriter.write("Address: " + this.Address+"\n");
+			bufferWriter.write("Bio: " + this.Bio+"\n");
+			while(( line = bufferReader.readLine())!= null)
+			{	
+				if(line.startsWith("Occupation: "))
+				{
+					Date birthdate = new SimpleDateFormat("dd-mm-yyyy").parse(this.Birthdate);
+					Calendar calendar = Calendar.getInstance();
+					calendar.setTime(birthdate);
+					
+					int year = calendar.get(Calendar.YEAR);
+					if((year >= 2000))
+					{
+						this.writeArrayList(this.Occupations, bufferWriter, "Occupation: ");
+					}					
+				}
+				else if(line.startsWith("Awards: "))
+				{
+					for(String award : this.Awards)
+					{
+						String[] newAwardParts = award.split(",");
+						String[]oldAwardParts = award.split(","):
+						if(newAwardParts.length == 2)
+						{
+							int year = Integer.parseInt(newAwardParts[0]);
+							if ((year >= 2000))
+							{
+								newAwardParts[0
+							}
+						}
+					}
+				}
+				else
+				{
+					bufferWriter.write(line);
+				}
+			}
+		}
+		
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		} 
+		catch (ParseException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		if(dataFromFile.contains(this.ID) && validDetails)
+		{
+			
+		}
+			return true;	
+	}
+	
+	public boolean Update()
+	{
+		Date converted_birthDate = new Date();
+		try {
+            FileWriter fileWriter = new FileWriter("fileName.txt", true);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            Scanner scanner = new Scanner(new File("fileName.txt"));
+            scanner.nextLine(); // Skip the first line
+
+            StringBuilder newContent = new StringBuilder();
+            String newContentString = "Occuption: ";
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("Award")) {
+                    String[] parts = line.substring(line.indexOf(": ") + 2).split("\\|");
+                    for (int i = 0; i < parts.length; i++)
+                    {
+                    	String part = parts[i];
+                    	String [] subParts = part.split(",");
+
+                        if (subParts.length == 2 && Integer.parseInt(subParts[0]) > 2000) {
+                        	subParts[1] = "new Award";
+                        }
+                        newContentString += String.join(",", subParts);
+                        newContent.append(String.join("|", newContentString)).append("\n");
+                    }
+                }
+
+                if (line.startsWith("Occupation")) 
+               {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                	try
+                	{
+                    converted_birthDate = dateFormat.parse(this.Birthdate);
+                	} 
+                	catch (ParseException e) 
+                	{
+                    // Handle parsing error
+                	}
+                	Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(converted_birthDate);
+                    if (calendar.get(Calendar.YEAR) > 2000) {
+                        newContent.append("new Occupation\n");
+                    }
+                } else {
+                    newContent.append(line).append("\n");
+                }
+            }
+
+            writer.write(newContent.toString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		return true;
+	}
+}
+
+
+//https://stackoverflow.com/questions/12970484/java-for-loop-with-an-arraylist
---
+++ SEF_MusicApp/src/ArtistTest.java
@@ -0,0 +1,224 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.stream.*;
+import org.junit.jupiter.params.provider.*;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class ArtistTest 
+{
+	////Valid input
+	  static Stream<Arguments> testDataOfCase1() 
+	  {		  
+			ArrayList<String> occupation = new ArrayList<String>();
+			occupation.add("Singer");
+			occupation.add("Developer");
+			occupation.add("Driver");
+			ArrayList<String> Genres = new ArrayList<String>();
+			Genres.add("pop");
+			Genres.add("Jazz");
+			ArrayList<String> Awards = new ArrayList<String>();
+			Awards.add("1999,69th film fare awards");
+			Awards.add("2002,69th film fare awards");
+	        return Stream.of(
+	            Arguments.of("567NNNRR_%", "Case1",  "Melbourne|Victoria|Australia","25-06-1999", "Sample bio for the test data 1 which contains 10 words", occupation,Genres, Awards),  
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", "Sample bio for the test data 2 which contains 10 words", occupation,Genres, Awards)  
+	        );
+	    }
+	  
+	  ////Invalid artist id
+	  static Stream<Arguments> testDataOfCase2() 
+	  {		  
+			ArrayList<String> occupation = new ArrayList<String>();
+			occupation.add("Singer");
+			occupation.add("Developer");
+			occupation.add("Driver");
+			ArrayList<String> Genres = new ArrayList<String>();
+			Genres.add("pop");
+			Genres.add("Jazz");
+			ArrayList<String> Awards = new ArrayList<String>();
+			Awards.add("1999,69th film fare awards");
+	        return Stream.of(
+	        	////Data where the ID is less than 10 characters
+	            Arguments.of("NNNRR", "Case1",  "Melbourne|Victoria|Australia","25-06-1999", "Sample bio for the test data 2 which contains 10 words", occupation,Genres, Awards),  
+	            
+	            ////Data where the ID has only two digits at the starting of string
+	            Arguments.of("12NNNRR@# ", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", "Sample bio for the test data 2 which contains 10 words", occupation,Genres, Awards),  
+	            
+	            ////Data where the ID has no upper case letters from 4th to 8th character
+	            Arguments.of("589nnnrr@# ", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", "Sample bio for the test data 2 which contains 10 words", occupation,Genres, Awards)  
+	        );
+	    }
+	  
+	  ////Invalid birth date
+	  static Stream<Arguments> testDataOfCase3() 
+	  {		  
+			ArrayList<String> occupation = new ArrayList<String>();
+			occupation.add("Singer");
+			occupation.add("Developer");
+			occupation.add("Driver");
+			ArrayList<String> Genres = new ArrayList<String>();
+			Genres.add("pop");
+			Genres.add("Jazz");
+			ArrayList<String> Awards = new ArrayList<String>();
+			Awards.add("1999,69th film fare awards");
+	        return Stream.of(
+	        	////Data where the date format of birth date is 'yyyy-mm-dd'
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","1999-06-25", "Sample bio for the test data 2 which contains 10 words", occupation,Genres, Awards),  
+	            
+	            ////Data where the the date format of birth date is 'dd-mmm-yyyy'
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-Jun-2023", "Sample bio for the test data 2 which contains 10 words", occupation,Genres, Awards)
+	         );
+	    }
+	  
+	  ////Invalid bio
+	  static Stream<Arguments> testDataOfCase4() 
+	  {		  
+			ArrayList<String> occupation = new ArrayList<String>();
+			occupation.add("Singer");
+			occupation.add("Developer");
+			occupation.add("Driver");
+			ArrayList<String> Genres = new ArrayList<String>();
+			Genres.add("pop");
+			Genres.add("Jazz");
+			ArrayList<String> Awards = new ArrayList<String>();
+			Awards.add("1999,69th film fare awards");
+			String bio = "Sample bio for the test data 2 which contains more than thirty words so that the test case will be passed and the function will validate and the condition fails which will leads or makes the function to return false as the return value of add artist function";
+	        return Stream.of(
+	        	////Data where the Bio of artist contains less than 10 words
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-1999", "Sample bio", occupation,Genres, Awards),  
+	            
+	            ////Data where the bio of artist contains more than 30 words
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", bio, occupation,Genres, Awards)  
+	        );
+	    }
+	  
+	  	////Invalid Awards
+		static Stream<Arguments> testDataOfCase5() 
+		{		  
+			ArrayList<String> occupation = new ArrayList<String>();
+			occupation.add("Singer");
+			occupation.add("Developer");
+			occupation.add("Driver");
+			ArrayList<String> Genres = new ArrayList<String>();
+			Genres.add("pop");
+			Genres.add("Jazz");
+			ArrayList<String> AwardsCase1 = new ArrayList<String>();
+			AwardsCase1.add("1999,SiMA awards");
+			ArrayList<String> AwardsCase2 = new ArrayList<String>();
+			AwardsCase2.add("1999,Best playback singer of the year");
+			AwardsCase2.add("2002,best voice of the year");
+			AwardsCase2.add("2005,sensational singer of the year");
+			AwardsCase2.add("2000,Best music composition of the year");
+			ArrayList<String> AwardsCase3 = new ArrayList<String>();
+			AwardsCase3.add("SIMA music fare awards");
+			
+	        return Stream.of(
+	        	////Data where the Award title of artist contains less than  words
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-1999", "Sample bio for the test data 1 which contains 10 words ", occupation,Genres, AwardsCase1),  
+	            
+	            ////Data where the artist doesn't have any awards
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", "Sample bio for the test data 2 which contains 10 words ", occupation,Genres, AwardsCase2),  
+	        
+	            ////Data where the award of the artist doesn't have a year.
+	            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", "Sample bio for the test data 3 which contains 10 words ", occupation,Genres, AwardsCase3)  
+	        
+	        		
+    		);
+	    }
+		
+		 ////Invalid Active Genres
+		  static Stream<Arguments> testDataOfCase6() 
+		  {		  
+				ArrayList<String> occupation = new ArrayList<String>();
+				occupation.add("Singer");
+				occupation.add("Developer");
+				occupation.add("Driver");
+				ArrayList<String> GenresCase1 = new ArrayList<String>();
+				GenresCase1.add("pop");
+				ArrayList<String> GenresCase2 = new ArrayList<String>();
+				GenresCase2.add("pop");	
+				GenresCase2.add("rock");
+				ArrayList<String> Awards = new ArrayList<String>();
+				Awards.add("1999,69th film fare awards");
+
+		        return Stream.of(
+		        	////Data where there is only one active genre for the artist
+		            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-1999", "Sample bio for the test data 1 which contains 10 words", occupation,GenresCase1, Awards),  
+		            
+		            ////Data where the artist was active in both 'pop' and 'rock' genres.
+		            Arguments.of("987ABCDE@#", "Case1",  "Melbourne|Victoria|Australia","25-06-2023", "Sample bio for the test data 2 which contains 10 words", occupation,GenresCase2, Awards)  
+		        );
+		    }
+	 
+	
+	@ParameterizedTest
+	@MethodSource("testDataOfCase1")
+	@DisplayName("Test Add Artist with valid input")
+	void testAddArtist_TestCase1(String ArtistID,  String Name, String Address, String Birthdate,String Bio, ArrayList<String> Occupations, ArrayList<String> Genres, ArrayList<String> Awards) 
+	{
+		Artist TestData1 = new Artist(ArtistID, Name, Address, Birthdate, Bio, Occupations, Genres, Awards);
+		boolean data = TestData1.Update();
+	//	boolean data = TestData1.addArtist();
+		Assert.assertTrue(data == true);
+	}
+	
+	
+	@ParameterizedTest
+	@MethodSource("testDataOfCase2")
+	@DisplayName("Test Add Artist with invalid Artist ID")
+	void testAddArtist_TestCase2(String ArtistID,  String Name, String Address, String Birthdate,String Bio, ArrayList<String> Occupations, ArrayList<String> Genres, ArrayList<String> Awards) 
+	{
+		Artist TestData1 = new Artist(ArtistID, Name, Address, Birthdate, Bio, Occupations, Genres, Awards);
+		boolean data = TestData1.addArtist();
+		Assert.assertTrue(data == false);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("testDataOfCase3")
+	@DisplayName("Test Add Artist with invalid date format of birthdate")
+	void testAddArtist_TestCase3(String ArtistID,  String Name, String Address, String Birthdate,String Bio, ArrayList<String> Occupations, ArrayList<String> Genres, ArrayList<String> Awards) 
+	{
+		Artist TestData1 = new Artist(ArtistID, Name, Address, Birthdate, Bio, Occupations, Genres, Awards);
+		boolean data = TestData1.addArtist();
+		Assert.assertTrue(data == false);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("testDataOfCase4")
+	@DisplayName("Test Add Artist with invalid bio")
+	void testAddArtist_TestCase4(String ArtistID,  String Name, String Address, String Birthdate,String Bio, ArrayList<String> Occupations, ArrayList<String> Genres, ArrayList<String> Awards) 
+	{
+		Artist TestData1 = new Artist(ArtistID, Name, Address, Birthdate, Bio, Occupations, Genres, Awards);
+		boolean data = TestData1.addArtist();
+		Assert.assertTrue(data == false);
+	}
+
+	@ParameterizedTest
+	@MethodSource("testDataOfCase5")
+	@DisplayName("Test Add Artist with invalid awards")
+	void testAddArtist_TestCase5(String ArtistID,  String Name, String Address, String Birthdate,String Bio, ArrayList<String> Occupations, ArrayList<String> Genres, ArrayList<String> Awards) 
+	{
+		Artist TestData1 = new Artist(ArtistID, Name, Address, Birthdate, Bio, Occupations, Genres, Awards);
+		boolean data = TestData1.addArtist();
+		Assert.assertTrue(data == false);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("testDataOfCase6")
+	@DisplayName("Test Add Artist with invalid sctive genres")
+	void testAddArtist_TestCase6(String ArtistID,  String Name, String Address, String Birthdate,String Bio, ArrayList<String> Occupations, ArrayList<String> Genres, ArrayList<String> Awards) 
+	{
+		Artist TestData1 = new Artist(ArtistID, Name, Address, Birthdate, Bio, Occupations, Genres, Awards);
+		boolean data = TestData1.addArtist();
+		Assert.assertTrue(data == false);
+	}
+
+}
+
+//JUnit 5 user guide
+//@Display for uni test check with it
